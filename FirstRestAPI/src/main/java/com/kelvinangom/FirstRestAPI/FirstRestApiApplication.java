d9b7d6f1529a54d8858f01711758dc1779ad7431
Angom <Kelvin.Angom@auspost.com.au>
2019-05-06 16:53:16 +1000

    Initial

---
+++ FirstRestAPI/src/main/java/com/kelvinangom/FirstRestAPI/FirstRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.kelvinangom.FirstRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestApiApplication.class, args);
+		//System.out.println("Hello");
+	}
+
+}
